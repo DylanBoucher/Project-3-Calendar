fb5a5fd3ef9eb2e57ecddd8c75577d15a22f8020
DylanBoucher <dylanboucher99@gmail.com>
2022-05-16 10:35:01 -0500

    added base calendar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Calendar from 'react-calendar';
+import React, { useState } from 'react'
 
 function App() {
+  const [date, setDate] = useState(new Date())
+
+  const onChange = (date) => {
+    setDate(date)
+  }
+
   return (
     <div className="App">
-      <h1>App</h1>
+      <Calendar onChange={onChange} value={date} />
+      {console.log(date)}
+      {/* <p>{date.toString()}</p> */}
     </div>
   );
 }
