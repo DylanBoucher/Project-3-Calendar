4a2861e38b6ecc3ffdc681e0cfcd25cc41f144c0
DylanBoucher <dylanboucher99@gmail.com>
2022-05-16 13:53:54 -0500

    add styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
-import Calendar from 'react-calendar';
-import React, { useState } from 'react'
+
+import Mycalendar from './Calender';
 
 function App() {
-  const [date, setDate] = useState(new Date())
-
-  const onChange = (date) => {
-    setDate(date)
-  }
+  
 
   return (
     <div className="App">
-      <Calendar onChange={onChange} value={date} />
-      {console.log(date)}
-      {/* <p>{date.toString()}</p> */}
+      <Mycalendar/>
     </div>
   );
 }
